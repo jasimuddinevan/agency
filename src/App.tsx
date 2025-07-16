db61cffca61a23efca25428a211ce6f8cebcb1f6
Jasim Uddin <101209751+jasimuddinevan@users.noreply.github.com>
2025-07-17 00:15:09 +0600

    Fix Navigate import error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ClientAuthProvider } from './contexts/ClientAuthContext';
 import Layout from './components/Layout';
